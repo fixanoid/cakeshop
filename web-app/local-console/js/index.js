774a6442c306d265ac4d1b905ebb8fe603f93721
Felix Shnir <fixanoid@Felixs-MacBook-Pro.local>
2015-11-23 11:02:17 -0500

    Console cleanup

--- web-app/local-console/js/index.js
+++ web-app/local-console/js/index.js
@@ -1,3 +1,7 @@
+var Tower = {};
+
+
+
 $(function() {
 	$(window).on("scroll", function(e) {
 		if ($(window).scrollTop() > 50) {
@@ -9,38 +13,40 @@ $(function() {
 
 	$(document).on("click", function(e) {
 		e.preventDefault();
+
 		var $item = $(".rad-dropmenu-item");
 		if ($item.hasClass("active")) {
 			$item.removeClass("active");
 		}
 	});
 
-	$('.rad-chat-body').slimScroll({
-		height: '450px',
-		color: "#c6c6c6"
-	});
+	// $('.rad-chat-body').slimScroll({
+	// 	height: '450px',
+	// 	color: "#c6c6c6"
+	// });
 
-	$('.rad-timeline-body').slimScroll({
-		height: '450px',
-		color: '#c6c6c6'
-	});
 
+	// Menu (burger) handler
 	$(".rad-toggle-btn").on('click', function() {
 		$(".rad-logo-container").toggleClass("rad-nav-min");
 		$(".rad-sidebar").toggleClass("rad-nav-min");
 		$(".rad-body-wrapper").toggleClass("rad-nav-min");
+
 		setTimeout(function() {
 
 		}, 200);
 	});
 
+	// Theme handler / switcher
 	$("li.rad-dropdown > a.rad-menu-item").on('click', function(e) {
 		e.preventDefault();
 		e.stopPropagation();
+
 		$(".rad-dropmenu-item").removeClass("active");
 		$(this).next(".rad-dropmenu-item").toggleClass("active");
 	});
 
+	// Widget collapse / expand handler
 	$(".fa-chevron-down").on("click", function() {
 		var $ele = $(this).parents('.panel-heading');
 		$ele.siblings('.panel-footer').toggleClass("rad-collapse");
@@ -51,14 +57,17 @@ $(function() {
 		});
 	});
 
+	// Widget close handler
 	$(".fa-close").on("click", function() {
 		var $ele = $(this).parents('.panel');
 		$ele.addClass('panel-close');
+
 		setTimeout(function() {
 			$ele.parent().remove();
 		}, 210);
 	});
 
+	// Widget refresh handler
 	$(".fa-rotate-right").on("click", function() {
 		var $ele = $(this).parents('.panel-heading').siblings('.panel-body');
 		$ele.append('<div class="overlay"><div class="overlay-content"><i class="fa fa-refresh fa-2x fa-spin"></i></div></div>');
@@ -67,33 +76,13 @@ $(function() {
 		}, 2000);
 	});
 
-	$("#rad-chat-send").on("click", function() {
-		var value = $("#rad-chat-txt").val();
-		var $ele = $(".rad-chat-body");
-		var img = "https://lh4.googleusercontent.com/-GXmmnYTuWkg/AAAAAAAAAAI/AAAAAAAAAAA/oK6DEDS7grM/w56-h56/photo.jpg";
-		if (value) {
-			$("#rad-chat-txt").val('');
-			$ele.append(getTempl(img, value, 'left'));
-			setTimeout(function() {
-				img = "http://www.gravatar.com/avatar/9099c2946891970eb4739e6455400913.png";
-				$ele.append(getTempl(img, "Cool!!!", 'right'));
-				$ele.slimScroll({
-					scrollTo: $ele[0].scrollHeight
-				});
-			}, 2000);
-
-			$ele.slimScroll({
-				scrollTo: $ele[0].scrollHeight
-			});
-
-		}
-	});
-
+	// Widget collapse / expand handler
 	$('.rad-chk-pin input[type=checkbox]').change(function(e) {
 		$('body').toggleClass("flat-theme");
 		$("#rad-color-opts").toggleClass("hide");
 	});
 
+	// Checkbox handlers
 	$('.rad-color-swatch input[type=radio]').change(function(e) {
 		if ($('.rad-chk-pin input[type=checkbox]').is(":checked")){
 			$('body').removeClass().addClass("flat-theme").addClass(this.value);
@@ -109,23 +98,11 @@ $(function() {
 		e.stopPropagation();
 	});
 
+
+	// Resize handler
 	$(window).resize(function() {
 		setTimeout(function() {
 
 		}, 200);
 	});
-
-	function getTempl(img, text, position) {
-		return '<div class="rad-list-group-item ' + position + '"><span class="rad-list-icon pull-' + position + '"><img class="rad-list-img" src=' + img + ' alt="me" /></span><div class="rad-list-content rad-chat"><span class="lg-text">Me</span><span class="sm-text"><i class="fa fa-clock-o"></i> ' + formatTime(new Date()) + '</span><div class="rad-chat-msg">' + text + '</div>';
-	}
-
-	function formatTime(date) {
-		var hours = date.getHours();
-		var minutes = date.getMinutes();
-		var ampm = hours >= 12 ? 'pm' : 'am';
-		hours = hours % 12;
-		hours = hours ? hours : 12;
-		minutes = minutes < 10 ? '0' + minutes : minutes;
-		return hours + ':' + minutes + ' ' + ampm;
-	}
 });
---
+++ web-app/local-console/js/templates.js
@@ -0,0 +1,59 @@
+Tower.TEMPLATES = {};
+
+Tower.TEMPLATES._widget = function(opts) {
+	return '<div class="col-lg-' + opts.largeColumn + ' col-md-' + opts.mediumColumn + ' col-xs-' + opts.smallColumn + '">\n'+
+			'	<div class="panel panel-default">\n'+
+			'		<div class="panel-heading">\n'+
+			'			<h3 class="panel-title">' + opts.title + '\n'+
+			'				<ul class="rad-panel-action">\n'+
+			'					<li><i class="fa fa-chevron-down"></i></li>\n'+
+			'					<li><i class="fa fa-rotate-right"></i></li>\n'+
+			'					<li><i class="fa fa-close"></i></li>\n'+
+			'				</ul>\n'+
+			'			</h3>\n'+
+			'		</div>\n'+
+			'		<div class="panel-body" id="widget-' + opts.id + '">\n'+
+			'		</div>\n'+
+			'	</div>\n'+
+			'</div>'
+}
+
+
+Tower.TEMPLATES.widget = function(title, size) {
+	var opts = {
+		title: title,
+		id: Math.ceil(Math.random() * 100000000),
+
+		largeColumn: 3,
+		mediumColumn: 4,
+		smallColumn: 12
+	};
+
+	switch (size) {
+		case 'medium':
+			opts.largeColumn = 6;
+			opts.mediumColumn = 12;
+			opts.smallColumn = 12;
+
+			break;
+
+		case 'large':
+			opts.largeColumn = 12;
+			opts.mediumColumn = 6;
+			opts.smallColumn = 12;
+
+			break;
+
+		case 'third':
+			opts.largeColumn = 4;
+			opts.mediumColumn = 6;
+			opts.smallColumn = 12;
+
+			break;
+	}
+
+	return {,
+		id: opts.id,
+		tpl: Tower.TEMPLATES._widget(opts)
+	};
+}
