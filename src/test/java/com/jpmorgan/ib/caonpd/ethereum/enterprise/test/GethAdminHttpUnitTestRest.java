673672b4cad5aa6e70d9018cf41a2574fde726f2
Samer Falah <samer.falah@jpchase.com>
2015-11-27 10:39:55 -0500

    added failure tests

--- src/test/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/test/GethAdminHttpUnitTestRest.java
+++ src/test/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/test/GethAdminHttpUnitTestRest.java
@@ -5,6 +5,8 @@
  */
 package com.jpmorgan.ib.caonpd.ethereum.enterprise.test;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import static org.springframework.http.MediaType.*;
 
 import org.springframework.http.HttpHeaders;
@@ -23,6 +25,7 @@ import org.springframework.beans.factory.annotation.Value;
 
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.bean.AdminBean;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.config.TestWebConfig;
+import org.springframework.util.Assert;
 
 
 /**
@@ -40,44 +43,44 @@ public class GethAdminHttpUnitTestRest extends AbstractTestNGSpringContextTests 
     @Value("${geth.genesis}")
     private String genesis;
     
+    private static JsonParser jsonParser;
+    
 
     @BeforeTest
     public static void setEnv() {
         System.setProperty("eth.environment", "local");
+        jsonParser = new JsonParser();
     }
 
     @AfterClass
     public static void afterClass() {
         System.clearProperty("eth.environment");
+        jsonParser = null;
     }
 
 
-    @Test(enabled = false)
-    //To run test - comment out (enabled = false) from Test annotation
+    @Test(enabled = true)
     public void testNodeInfo() {
-        //Change to whatever url your local tomcat  is set
-        //Webapp must be up and running to execute this test!!!
         String url = NODE_URL+AdminBean.ADMIN_NODE_INFO_KEY;
-        //String funcName = "admin_peers";
         String funcArgs = " ";
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MULTIPART_FORM_DATA);
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        //body.add("func_name", funcName);
+
         body.add("func_args", funcArgs);
         String result = restTemplate.postForObject(url, body, String.class);
-        System.out.println("Geth func call response :" + result);
+        Assert.notNull(result);
+        System.out.println("Geth func call Node Info :" + result);
+        JsonObject jsonResult = jsonParser.parse(result).getAsJsonObject();
+        Assert.notNull(jsonResult.get("result"));
 
     }
 
 
     @Test(enabled = false)
-    //To run test - comment out (enabled = false) from Test annotation
     public void testNodePeers() {
-        //Change to whatever url your local tomcat  is set
-        //Webapp must be up and running to execute this test!!!
         String url = NODE_URL+AdminBean.ADMIN_PEERS_KEY;
         String funcArgs = " ";
         RestTemplate restTemplate = new RestTemplate();
@@ -85,18 +88,19 @@ public class GethAdminHttpUnitTestRest extends AbstractTestNGSpringContextTests 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MULTIPART_FORM_DATA);
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        //body.add("func_name", funcName);
         body.add("func_args", funcArgs);
         String result = restTemplate.postForObject(url, body, String.class);
-        System.out.println("Geth func call response :" + result);
+        Assert.notNull(result);
+        System.out.println("Geth func call node peers :" + result);
+        JsonObject jsonResult = jsonParser.parse(result).getAsJsonObject();
+        Assert.notNull(jsonResult.get("result"));
 
     }
     
     @Test(enabled = false)
-    //To run test - comment out (enabled = false) from Test annotation
+
     public void testNodeVerbosity() {
-        //Change to whatever url your local tomcat  is set
-        //Webapp must be up and running to execute this test!!!
+
         String url = NODE_URL+AdminBean.ADMIN_VERBOSITY_KEY;
         String funcArgs = "0";
         RestTemplate restTemplate = new RestTemplate();
@@ -104,18 +108,20 @@ public class GethAdminHttpUnitTestRest extends AbstractTestNGSpringContextTests 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MULTIPART_FORM_DATA);
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        //body.add("func_name", funcName);
         body.add("func_args", funcArgs);
         String result = restTemplate.postForObject(url, body, String.class);
-        System.out.println("Geth func call response :" + result);
+        Assert.notNull(result);
+        System.out.println("Geth func call node verbosity :" + result);
+        JsonObject jsonResult = jsonParser.parse(result).getAsJsonObject();
+        Assert.notNull(jsonResult.get("result"));
+        Assert.isTrue(Boolean.parseBoolean(jsonResult.get("result").toString()));
 
     }
 
     @Test(enabled = false)
     //To run test - comment out (enabled = false) from Test annotation
     public void testNodeDatadir() {
-        //Change to whatever url your local tomcat  is set
-        //Webapp must be up and running to execute this test!!!
+
         String url = NODE_URL+AdminBean.ADMIN_DATADIR_KEY;
         String funcArgs = " ";
         RestTemplate restTemplate = new RestTemplate();
@@ -123,18 +129,19 @@ public class GethAdminHttpUnitTestRest extends AbstractTestNGSpringContextTests 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MULTIPART_FORM_DATA);
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        //body.add("func_name", funcName);
         body.add("func_args", funcArgs);
         String result = restTemplate.postForObject(url, body, String.class);
-        System.out.println("Geth func call response :" + result);
+        Assert.notNull(result);
+        System.out.println("Geth func call node Datadir :" + result);
+        JsonObject jsonResult = jsonParser.parse(result).getAsJsonObject();
+        Assert.notNull(jsonResult.get("result"));
 
     }
   
-   @Test(enabled = true)
+   @Test(enabled = false)
     //To run test - comment out (enabled = false) from Test annotation
     public void testMinerStart() {        
-        //Change to whatever url your local tomcat  is set
-        //Webapp must be up and running to execute this test!!!
+
         String url = MINER_URL+AdminBean.ADMIN_MINER_START_KEY;
         String funcArgs = " ";
         RestTemplate restTemplate = new RestTemplate();
@@ -142,18 +149,20 @@ public class GethAdminHttpUnitTestRest extends AbstractTestNGSpringContextTests 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MULTIPART_FORM_DATA);
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        //body.add("func_name", funcName);
+        
         body.add("func_args", funcArgs);
         String result = restTemplate.postForObject(url, body, String.class);
-        System.out.println("Miner func call response :" + result);
+        Assert.notNull(result);
+        JsonObject jsonResult = jsonParser.parse(result).getAsJsonObject();
+        Assert.isNull(jsonResult.get("error"));
+        System.out.println("Miner func call miner start :" + result);
 
     }
     
     @Test(enabled = false)
     //To run test - comment out (enabled = false) from Test annotation
     public void testMinerStop() {        
-        //Change to whatever url your local tomcat  is set
-        //Webapp must be up and running to execute this test!!!
+
         String url = MINER_URL+AdminBean.ADMIN_MINER_STOP_KEY;
         String funcArgs = " ";
         RestTemplate restTemplate = new RestTemplate();
@@ -161,17 +170,20 @@ public class GethAdminHttpUnitTestRest extends AbstractTestNGSpringContextTests 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MULTIPART_FORM_DATA);
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        //body.add("func_name", funcName);
         body.add("func_args", funcArgs);
         String result = restTemplate.postForObject(url, body, String.class);
-        System.out.println("Miner func call response :" + result);
+        Assert.notNull(result);
+        System.out.println("Miner func call miner stop :" + result);
+        JsonObject jsonResult = jsonParser.parse(result).getAsJsonObject();
+        Assert.notNull(jsonResult.get("result"));
+        Assert.isTrue(Boolean.parseBoolean(jsonResult.get("result").toString()));
+        
     }
     
     @Test(enabled = false)
     //To run test - comment out (enabled = false) from Test annotation
     public void testMinerStatus() {        
-        //Change to whatever url your local tomcat  is set
-        //Webapp must be up and running to execute this test!!!
+
         String url = MINER_URL+AdminBean.ADMIN_MINER_MINING_KEY;
         String funcArgs = " ";
         RestTemplate restTemplate = new RestTemplate();
@@ -179,10 +191,12 @@ public class GethAdminHttpUnitTestRest extends AbstractTestNGSpringContextTests 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MULTIPART_FORM_DATA);
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        //body.add("func_name", funcName);
         body.add("func_args", funcArgs);
         String result = restTemplate.postForObject(url, body, String.class);
-        System.out.println("Miner func call response :" + result);
+        Assert.notNull(result);
+        System.out.println("Miner func call miner status :" + result);
+        JsonObject jsonResult = jsonParser.parse(result).getAsJsonObject();
+        Assert.notNull(jsonResult.get("result"));
     }
 
 }
