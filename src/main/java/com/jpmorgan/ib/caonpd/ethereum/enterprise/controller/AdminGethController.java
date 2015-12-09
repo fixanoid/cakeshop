54997293e5ed29ed521b85b2817680d1fdd1dca9
Michael Kazansky <I629630@10.87.4.105>
2015-12-09 12:39:06 -0500

    code to update node info

--- src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/controller/AdminGethController.java
+++ src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/controller/AdminGethController.java
@@ -5,7 +5,6 @@
  */
 package com.jpmorgan.ib.caonpd.ethereum.enterprise.controller;
 
-
 import com.google.gson.Gson;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.bean.AdminBean;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.config.JsonMethodArgumentResolver.JsonBodyParam;
@@ -14,9 +13,9 @@ import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.Node;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.RequestModel;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.service.GethHttpService;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.service.NodeService;
+import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -24,57 +23,90 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
  * @author N631539
  */
-
 @Controller
 public class AdminGethController {
-    
+
     @Autowired
-    private GethHttpService gethService;    
-    
+    private GethHttpService gethService;
+
     @Autowired
     private NodeService nodeService;
-    
+
     @Autowired
     private AdminBean adminBean;
-    
-    @RequestMapping(value = {"/node/{funcName}","/miner/{funcName}"}, method = POST,produces = MediaType.APPLICATION_JSON_VALUE)
-    protected @ResponseBody String adminFuncCall(@PathVariable String funcName, @JsonBodyParam (value = "args", required = false) String funcArguments) throws APIException {
-        
-        String args [] = null;
+
+    @RequestMapping(value = {"/node/{funcName}", "/miner/{funcName}"}, method = POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    protected @ResponseBody
+    String adminFuncCall(@PathVariable String funcName, @JsonBodyParam(value = "args", required = false) String funcArguments) throws APIException {
+
+        String args[] = null;
         String response = null;
         RequestModel request = null;
         Node node = null;
-        
-        if(StringUtils.isNotEmpty(funcName) && funcName.equalsIgnoreCase("status")){
+
+        if (StringUtils.isNotEmpty(funcName) && funcName.equalsIgnoreCase("status")) {
             node = nodeService.get();
-            response = (node==null?new Node().toString():node.toString());
+            response = (node == null ? new Node().toString() : node.toString());
             return response;
         }
-        
+
         if (StringUtils.isNotEmpty(funcArguments)) {
             args = funcArguments.split(",");
-        }else if(AdminBean.ADMIN_MINER_START_KEY.equalsIgnoreCase(funcName)){
+        } else if (AdminBean.ADMIN_MINER_START_KEY.equalsIgnoreCase(funcName)) {
             args = new String[]{"1"};//set default to one cpu
         }
         
-        Map<String,String> functionNames = adminBean.getFunctionNames();
-        String gethFunctionName = functionNames.get(funcName); 
-        
-        if(StringUtils.isNotEmpty(gethFunctionName)){
+        String gethFunctionName = adminBean.getFunctionNames().get(funcName);
+
+        if (StringUtils.isNotEmpty(gethFunctionName)) {
             request = new RequestModel(GethHttpService.GETH_API_VERSION, gethFunctionName, args, GethHttpService.USER_ID);
-        } 
+        }
         Gson gson = new Gson();
-        if (request != null)
-         response = gethService.executeGethCall(gson.toJson(request));
+        if (request != null) {
+            response = gethService.executeGethCall(gson.toJson(request));
+        }
         return response;
     }
-    
-    
+
+    @RequestMapping(value = {"/node/update"}, method = POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    protected @ResponseBody
+    String updateNodeInfo(@JsonBodyParam(required = false) String verbosity,
+            @JsonBodyParam(required = false) String identity,
+            @JsonBodyParam(required = false) String mining,
+            @JsonBodyParam(required = false) String networkid) {
+        Map<String, String> newProps = new HashMap();
+        if (StringUtils.isNotEmpty(mining)) {
+            newProps.put("geth.mining", mining);
+        }
+        if (StringUtils.isNotEmpty(identity)) {
+            newProps.put("geth.identity", identity);
+        }
+        if (StringUtils.isNotEmpty(verbosity)) {
+            newProps.put("geth.verbosity", verbosity);
+        }
+        if(StringUtils.isNotEmpty(networkid)) {
+            newProps.put("geth.networkid",networkid);
+        }
+
+        if (newProps.size() > 0) {
+            nodeService.updateNodeInfo(newProps);
+            return "Node Updated";
+        } else {
+            return "Params are empty. Node has not been updated";
+        }
+    }
+
+    @RequestMapping(value = {"/node/reset"}, method = POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    protected @ResponseBody
+    String resetNodeInfo() {
+        Boolean reset = nodeService.resetNodeInfo();
+        return String.valueOf(reset);
+    }
+
 }
--- src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/service/impl/NodeServiceImpl.java
+++ src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/service/impl/NodeServiceImpl.java
@@ -1,31 +1,35 @@
 package com.jpmorgan.ib.caonpd.ethereum.enterprise.service.impl;
 
-import com.google.gson.Gson;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.bean.AdminBean;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.error.APIException;
-
-
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-
+import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.Node;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.service.GethHttpService;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.service.NodeService;
-import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.Node;
-import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.RequestModel;
-import java.net.ConnectException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service; 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 @Service
 public class NodeServiceImpl implements NodeService {
     
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(GethHttpServiceImpl.class);
 
     @Autowired
     private GethHttpService gethService;
+    
 
     @Override
     public Node get(){
@@ -33,37 +37,77 @@ public class NodeServiceImpl implements NodeService {
         Object input = null;
         Node node = new Node();
 
-        Map<String, Object> data=null;
+        Map<String, Object> data = null;
         
         try {
             //check if node is available 
             data = gethService.executeGethCall(AdminBean.ADMIN_NODE_INFO, new Object[]{ input, true });
-            node.setStatus(StringUtils.isEmpty((String)data.get("NodeID"))?NodeService.NODE_NOT_RUNNING_STATUS:NodeService.NODE_RUNNING_STATUS);
+            node.setStatus(StringUtils.isEmpty((String)data.get("NodeID")) ? NodeService.NODE_NOT_RUNNING_STATUS : NodeService.NODE_RUNNING_STATUS);
             //check if mining
             data = gethService.executeGethCall(AdminBean.ADMIN_MINER_MINING, new Object[]{ input, true });
             Boolean mining = (Boolean)data.get("id");
-            node.setMining(mining==null?false:mining);
+            node.setMining(mining == null ? false : mining);
             //peer count
             data = gethService.executeGethCall(AdminBean.ADMIN_NET_PEER_COUNT, new Object[]{ input, true });
             String peerCount = (String)data.get("id");
-            node.setPeerCount(peerCount==null?0:Integer.decode(peerCount));
+            node.setPeerCount(peerCount == null ? 0 : Integer.decode(peerCount));
             //get last block number
             data = gethService.executeGethCall(AdminBean.ADMIN_ETH_BLOCK_NUMBER, new Object[]{ input, true });
             String blockNumber = (String)data.get("id");
-            node.setLatestBlock(blockNumber==null?0:Integer.decode(blockNumber));
+            node.setLatestBlock(blockNumber == null ? 0 : Integer.decode(blockNumber));
             //get pending transactions
             data = gethService.executeGethCall(AdminBean.ADMIN_TXPOOL_STATUS, new Object[]{ input, true });
             Integer pending = (Integer)data.get("pending");
-            node.setPendingTxn(pending==null?0:pending);
-        } catch (APIException ex) {
+            node.setPendingTxn(pending == null ? 0 : pending);
+        } catch (APIException | NumberFormatException ex) {
             node.setStatus(NodeService.NODE_NOT_RUNNING_STATUS);
-            Logger.getLogger(NodeServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        } catch(Exception ex){
-            node.setStatus(NodeService.NODE_NOT_RUNNING_STATUS);
-            Logger.getLogger(NodeServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            LOG.error(ex.getMessage());
         }
         
         return node;
+    }
+
+    @Override
+    public void updateNodeInfo(Map<String, String> newProps) {
+        String prpsPath = this.getClass().getClassLoader().getResource("").getPath().replace("/WEB-INF/classes/", "") + File.separator
+                + ".." + File.separator + "env.properties";
+        try {
+            InputStream input = new FileInputStream(prpsPath);
+            Properties props = new Properties();
+            props.load(input);
+            Boolean needUpdate = false;
+            for (String key : newProps.keySet()) {
+                if (StringUtils.isNotEmpty(props.getProperty(key))
+                        && !props.getProperty(key).equalsIgnoreCase(newProps.get(key))) {
+                    props.setProperty(key, newProps.get(key));
+                    needUpdate = true;
+                }
+            }
+            if (needUpdate) {
+                try (FileOutputStream out = new FileOutputStream(prpsPath)) {
+                    props.store(out, null);
+                }
+                restart();
+            }
+        } catch (IOException ex) {
+            LOG.error(ex.getMessage());
+        }
+
+    }
+
+    @Override
+    public Boolean resetNodeInfo() {
+        String prpsPath = this.getClass().getClassLoader().getResource("").getPath().replace("/WEB-INF/classes/", "") + File.separator
+                + ".." + File.separator + "env.properties";
+        Boolean deleted = new File(prpsPath).delete();
+        restart();
+        return deleted;
+    }
+    
+    private void restart() {
+        gethService.stopGeth();
+        gethService.deletePid();
+        gethService.start();
     }
    
 }
