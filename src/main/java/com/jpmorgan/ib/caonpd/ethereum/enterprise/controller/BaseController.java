2ba1cc3658cee9243bb9c9b18f06adddc719a131
Samer Falah <samer.falah@jpmchase.com>
2015-12-11 14:59:19 -0500

    updated exception handling

--- src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/controller/BaseController.java
+++ src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/controller/BaseController.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.ResourceAccessException;
 
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.APIError;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.APIResponse;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -17,21 +18,23 @@ public class BaseController {
     public ResponseEntity<APIResponse> handleException(APIException ex) {
 
         APIResponse res = new APIResponse();
-        res.addError(new APIError("", HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Internal server error"));
+        String rootCause = ExceptionUtils.getRootCauseMessage((Throwable)ex);
+        res.addError(new APIError("", HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Internal server error",rootCause));
         return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    
+      
     @ExceptionHandler(Exception.class)
     public ResponseEntity<APIResponse> handleAllException(Exception ex) {
 
         APIResponse res = new APIResponse();
-
+        String rootCause = ExceptionUtils.getRootCauseMessage((Throwable)ex);
+        
         if (ex instanceof ResourceAccessException) {
             res.addError(new APIError(null, HttpStatus.SERVICE_UNAVAILABLE.toString(), ex.getMessage()));
             return new ResponseEntity<>(res, HttpStatus.SERVICE_UNAVAILABLE);
         }
 
-        res.addError(new APIError("", HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Internal server error"));
+        res.addError(new APIError("", HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Internal server error",rootCause));
         return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
