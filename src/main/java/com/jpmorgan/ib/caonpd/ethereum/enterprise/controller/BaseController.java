50111655ffc1ee4f8dcab79d5c3148460c8aa970
Chetan Sarva <chetan.sarva@jpmchase.com>
2015-12-17 15:19:57 -0500

    return more specific exceptions based on downstream errors

--- src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/controller/BaseController.java
+++ src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/controller/BaseController.java
@@ -1,42 +1,50 @@
 package com.jpmorgan.ib.caonpd.ethereum.enterprise.controller;
 
-import com.jpmorgan.ib.caonpd.ethereum.enterprise.error.APIException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 
+import com.jpmorgan.ib.caonpd.ethereum.enterprise.error.APIException;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.APIError;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.APIResponse;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.springframework.stereotype.Controller;
 
 @Controller
 public class BaseController {
 
-    @ExceptionHandler(APIException.class)
-    public ResponseEntity<APIResponse> handleException(APIException ex) {
-
-        APIResponse res = new APIResponse();
-        String rootCause = ExceptionUtils.getRootCauseMessage((Throwable)ex);
-        res.addError(new APIError("", HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Internal server error",rootCause));
-        return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-      
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<APIResponse> handleAllException(Exception ex) {
-
+    public ResponseEntity<APIResponse> handleException(APIException ex) {
         APIResponse res = new APIResponse();
-        String rootCause = ExceptionUtils.getRootCauseMessage((Throwable)ex);
-        
-        if (ex instanceof ResourceAccessException) {
-            res.addError(new APIError(null, HttpStatus.SERVICE_UNAVAILABLE.toString(), ex.getMessage()));
-            return new ResponseEntity<>(res, HttpStatus.SERVICE_UNAVAILABLE);
+        String rootCause = ExceptionUtils.getRootCauseMessage(ex);
+
+        if (ex instanceof APIException) {
+            // try to pass back more specific exceptions
+
+            Throwable cause = ex.getCause();
+            if (cause instanceof ResourceAccessException) {
+                // server down (failed connect)
+                res.addError(new APIError(null, HttpStatus.SERVICE_UNAVAILABLE.toString(), "Service unavailable", rootCause));
+                return new ResponseEntity<>(res, HttpStatus.SERVICE_UNAVAILABLE);
+
+            } else if (cause instanceof HttpClientErrorException) {
+                // server returned 4xx
+                HttpClientErrorException err = (HttpClientErrorException) cause;
+                res.addError(new APIError(null, err.getStatusCode().toString(), err.getStatusText(), rootCause));
+                return new ResponseEntity<>(res, err.getStatusCode());
+
+            } else if (cause instanceof HttpServerErrorException) {
+                // server returned 5xx
+                HttpServerErrorException err = (HttpServerErrorException) cause;
+                res.addError(new APIError(null, err.getStatusCode().toString(), err.getStatusText(), rootCause));
+                return new ResponseEntity<>(res, err.getStatusCode());
+            }
         }
 
-        res.addError(new APIError("", HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Internal server error",rootCause));
+        res.addError(new APIError(null, HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Internal server error", rootCause));
         return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-
     }
-
 }
