999e8f9fbaa8a2be9bd97302fc5b8db58954c457
Michael Kazansky <michael.kazansky@jpmchase.com>
2016-01-12 15:12:06 -0500

    1. WS - https://issuetracking.jpmchase.net/jira5/browse/NPDAPPS-2047 2. Updated pom file - latest version of spring and little housekeeping :) 3. Added js libraries for web sockets 4. Just launch test.html to see the beauty of web sockets. 5. Fix to exclude web socket from test

--- src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/config/WebSocketConfig.java
+++ src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/config/WebSocketConfig.java
@@ -43,7 +43,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         String [] endpoints = {"/node", "/block", "transaction"};
-        registry.addEndpoint(endpoints).withSockJS();
+        registry.addEndpoint("ws").withSockJS();
         
     }
     
