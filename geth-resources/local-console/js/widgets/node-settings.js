c2e844c4a95f2931bfb3f98339e7f2c40bdeca7a
fixanoid <felix.shnir@jpmorgan.com>
2016-01-20 13:43:32 -0500

    NPDAPPS-2101 Node Settings update

--- geth-resources/local-console/js/widgets/node-settings.js
+++ geth-resources/local-console/js/widgets/node-settings.js
@@ -9,55 +9,52 @@
 
 		template: _.template(
 			'  <div class="form-group">' +
-			'    <label for="networkid">Network ID</label>' +
-			'    <input type="text" class="form-control" id="networkid">' +
+			'    <label for="committing_transactions">Commiting Transactions</label>' +
+			'	 <select id="committing_transactions" class="form-control">' +
+			'	  <option value="true">Yes</option>' +
+			'	  <option value="false">No</option>' +
+			'	</select>' +
+			'  </div>' +
+			'  <div class="form-group">' +
+			'    <label for="network_id">Network ID</label>' +
+			'    <input type="text" class="form-control" id="network_id">' +
 			'  </div>' +
 			'  <div class="form-group">' +
 			'    <label for="identity">Identity</label>' +
 			'    <input type="text" class="form-control" id="identity">' +
 			'  </div>' +
 			'  <div class="form-group">' +
-			'    <label for="verbosity">Log Level</label>' +
-			'			<select id="verbosity" class="form-control">' +
-			'			  <option value="6">FINEST</option>' +
-			'			  <option value="5">FINER</option>' +
-			'			  <option value="4">FINE</option>' +
-			'			  <option value="3">INFO</option>' +
-			'			  <option value="2">WARNING</option>' +
-			'			  <option value="1">SEVERE</option>' +
-			'			</select>' +
-			'  </div>' +
-			'  <div class="form-group">' +
-			'    <label for="mining">Commiting Transactions</label>' +
-			'			<select id="mining" class="form-control">' +
-			'			  <option value="true">Yes</option>' +
-			'			  <option value="false">No</option>' +
-			'			</select>' +
-			'  </div>' +
-			'  <div class="pull-right form-group">' +
-			'    <a href="#"> <i class="fa fa-download"></i> Download Genesis Block</a>' +
+			'    <label for="log_level">Log Level</label>' +
+			'	<select id="log_level" class="form-control">' +
+			'	  <option value="6">TRACE</option>' +
+			'	  <option value="5">DEBUG</option>' +
+			'	  <option value="4">INFO</option>' +
+			'	  <option value="3">WARN</option>' +
+			'	  <option value="2">ERROR</option>' +
+			'	  <option value="1">FATAL</option>' +
+			'	</select>' +
 			'  </div>'),
+			// '  <div class="pull-right form-group">' +
+			// '    <a href="#"> <i class="fa fa-download"></i> Download Genesis Block</a>' +
+			// '  </div>'),
 
 		// this may be overwritten by main runner
 		ready: function() {
 			this.render();
 		},
 
-		url: {
-			info: '../api/node/settings',
-			update: '../api/node/settings/update',
-		},
+		url: '../api/node/update',
 
 		fetch: function() {
 			var _this = this;
 
 			$.when(
-				utils.load({ url: this.url.info })
+				utils.load({ url: this.url })
 			).done(function(info) {
-				$('#widget-' + _this.shell.id + ' #networkid').val( info.data.attributes.networkid ? info.data.attributes.networkid : '' );
+				$('#widget-' + _this.shell.id + ' #network_id').val( info.data.attributes.network_id ? info.data.attributes.network_id : '' );
 				$('#widget-' + _this.shell.id + ' #identity').val( info.data.attributes.identity ? info.data.attributes.identity : '' );
-				$('#widget-' + _this.shell.id + ' #verbosity').val( info.data.attributes.verbosity ? info.data.attributes.verbosity : '4' );
-				$('#widget-' + _this.shell.id + ' #mining').val( info.data.attributes.mining ? 'true' : 'false' );
+				$('#widget-' + _this.shell.id + ' #log_level').val( info.data.attributes.log_level ? info.data.attributes.log_level : '4' );
+				$('#widget-' + _this.shell.id + ' #committing_transactions').val( info.data.attributes.committing_transactions ? 'true' : 'false' );
 			});
 		},
 
@@ -90,7 +87,7 @@
 			data[action] = val;
 
 			$.when(
-				utils.load({ url: widget.url.update, data: data })
+				utils.load({ url: widget.url, data: data })
 			).done(function(info) {
 				// trigger event update
 				$(document).trigger('WidgetInternalEvent', [ widget.name + '|updated|' + action] );
