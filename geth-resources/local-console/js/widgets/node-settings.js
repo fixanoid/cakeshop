c31781f1d109e70965042a05b782c650610e711a
fixanoid <felix.shnir@jpmorgan.com>
2016-01-20 14:35:15 -0500

    updating parameteres for node settings

--- geth-resources/local-console/js/widgets/node-settings.js
+++ geth-resources/local-console/js/widgets/node-settings.js
@@ -9,23 +9,23 @@
 
 		template: _.template(
 			'  <div class="form-group">' +
-			'    <label for="committing_transactions">Commiting Transactions</label>' +
-			'	 <select id="committing_transactions" class="form-control">' +
+			'    <label for="committingTransactions">Commiting Transactions</label>' +
+			'	 <select id="committingTransactions" class="form-control">' +
 			'	  <option value="true">Yes</option>' +
 			'	  <option value="false">No</option>' +
 			'	</select>' +
 			'  </div>' +
 			'  <div class="form-group">' +
-			'    <label for="network_id">Network ID</label>' +
-			'    <input type="text" class="form-control" id="network_id">' +
+			'    <label for="networkId">Network ID</label>' +
+			'    <input type="text" class="form-control" id="networkId">' +
 			'  </div>' +
 			'  <div class="form-group">' +
 			'    <label for="identity">Identity</label>' +
 			'    <input type="text" class="form-control" id="identity">' +
 			'  </div>' +
 			'  <div class="form-group">' +
-			'    <label for="log_level">Log Level</label>' +
-			'	<select id="log_level" class="form-control">' +
+			'    <label for="logLevel">Log Level</label>' +
+			'	<select id="logLevel" class="form-control">' +
 			'	  <option value="6">TRACE</option>' +
 			'	  <option value="5">DEBUG</option>' +
 			'	  <option value="4">INFO</option>' +
@@ -51,10 +51,10 @@
 			$.when(
 				utils.load({ url: this.url })
 			).done(function(info) {
-				$('#widget-' + _this.shell.id + ' #network_id').val( info.data.attributes.network_id ? info.data.attributes.network_id : '' );
+				$('#widget-' + _this.shell.id + ' #networkId').val( info.data.attributes.networkId ? info.data.attributes.networkId : '' );
 				$('#widget-' + _this.shell.id + ' #identity').val( info.data.attributes.identity ? info.data.attributes.identity : '' );
-				$('#widget-' + _this.shell.id + ' #log_level').val( info.data.attributes.log_level ? info.data.attributes.log_level : '4' );
-				$('#widget-' + _this.shell.id + ' #committing_transactions').val( info.data.attributes.committing_transactions ? 'true' : 'false' );
+				$('#widget-' + _this.shell.id + ' #logLevel').val( info.data.attributes.logLevel ? info.data.attributes.logLevel : '4' );
+				$('#widget-' + _this.shell.id + ' #committingTransactions').val( info.data.attributes.committingTransactions ? 'true' : 'false' );
 			});
 		},
 
