c492c2baec93b1e6f1a59e9028636f46a6d7d685
Chetan Sarva <chetan.sarva@jpmchase.com>
2016-02-11 13:59:58 -0500

    [sandbox] fix window/editor resizing

--- src/main/webapp/js/sandbox/editor.js
+++ src/main/webapp/js/sandbox/editor.js
@@ -14,49 +14,50 @@
         session.setTabSize(2);
         session.setUseSoftTabs(true);
 
-        var maxWidth;
-        var editorHeight;
+        var fs = $(".container-fs"),
+            ed = $("#editor"),
+            sb = $("#sidebar");
 
-        function updateSizing() {
-            maxWidth = $(".container-fs").width()-400;
-            editorHeight = $(".container-fs").height() - $(".filetabs").height() - 5;
+        var SIDEBAR_MIN_WIDTH = 330;
+
+        function resizeEditor(width) {
+            // set Ace editor height
+            var editorHeight = fs.height() - $(".filetabs").height() - 5;
             $("#editor_input").height(editorHeight);
         	editor.resize();
             $(".sidebar .tab").height(editorHeight-10);
-        }
-        updateSizing();
 
-        // set default widths
-        var fs = $(".container-fs").width();
-        $("#editor").width(fs*0.7);
-        $("#sidebar").width(fs*0.3-7);
+            // set editor div width
+            width = width || (fs.width() - sb.width() - 30);
+            ed.width(width);
+            $("#editor").resizable("option", { maxWidth: fs.width() - SIDEBAR_MIN_WIDTH - 10 });
+        }
+
+        function resizeSidebar() {
+            var newWidth = fs.width() - ed.width();
+            if (newWidth < SIDEBAR_MIN_WIDTH) {
+                newWidth = SIDEBAR_MIN_WIDTH;
+            }
+            sb.width(newWidth - 30); // 30px room for paddings
+        }
 
         $(window).resize(function(e) {
-            updateSizing();
-          	if (this === e.target) {
-        		$('.sidebar').removeAttr('style');
+            if (e.target !== window) {
+                return;
             }
+            resizeEditor();
+            resizeSidebar();
         });
 
         $("#editor").resizable({
             handles: "e",
-        	minWidth: 200,
-            maxWidth: maxWidth,
-
-            resize: function(event, ui) {
-                var x=ui.element.outerWidth();
-                // var y=ui.element.outerHeight();
-                var ele=ui.element;
-                var factor = $(this).parent().width()-x;
-                var f2 = $(this).parent().width() * 0.02999;
-                $.each(ele.siblings(), function(idx, item) {
-                    //ele.siblings().eq(idx).css('height',y+'px');
-                    //ele.siblings().eq(idx).css('width',(factor-41)+'px');
-                    ele.siblings().eq(idx).width((factor-f2)+'px');
-                });
-            }
-
+        	minWidth: 500,
+            resize: resizeSidebar, // expand sidebar based on new editor width
         });
+
+        // set default widths
+        resizeEditor(fs.width() * 0.7); // set a default width to 70%
+        resizeSidebar();
 
         return editor;
     };
