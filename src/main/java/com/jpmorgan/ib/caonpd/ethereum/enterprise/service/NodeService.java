965e2c397cf441f554a49a72a9c5865c0dc19044
Chetan Sarva <chetan.sarva@jpmchase.com>
2016-02-23 14:57:24 -0500

    docs

--- src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/service/NodeService.java
+++ src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/service/NodeService.java
@@ -14,10 +14,41 @@ public interface NodeService {
     public String NODE_RUNNING_STATUS="running";
     public String NODE_NOT_RUNNING_STATUS="stopped";
 
+    /**
+     * Get node information
+     *
+     * @return {@link Node}
+     * @throws APIException
+     */
     public Node get() throws APIException;
-    public NodeInfo update(Integer logLevel,Integer networkID,String identity,Boolean mining) throws APIException;
+
+    /**
+     * Update node configuration (may trigger restart)
+     *
+     * @param logLevel
+     * @param networkID
+     * @param identity
+     * @param mining
+     * @return
+     * @throws APIException
+     */
+    public NodeInfo update(Integer logLevel, Integer networkID, String identity, Boolean mining) throws APIException;
+
     public APIData getAPIData(Map data);
+
+    /**
+     * Reset node back to default configuration (will restart)
+     *
+     * @return
+     */
     public Boolean reset();
+
+    /**
+     * Retrieve a list of connected peers
+     *
+     * @return
+     * @throws APIException
+     */
     public List<Peer> peers() throws APIException;
 
 }
