66b7219b1555b6820f8d0a9fc6fb934b047c1496
Chetan Sarva <chetan.sarva@jpmchase.com>
2016-04-18 11:45:49 -0400

    [perf] make block persistance non-blocking

--- src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/service/impl/SavingBlockListener.java
+++ src/main/java/com/jpmorgan/ib/caonpd/ethereum/enterprise/service/impl/SavingBlockListener.java
@@ -7,6 +7,11 @@ import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.Block;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.model.Transaction;
 import com.jpmorgan.ib.caonpd.ethereum.enterprise.service.TransactionService;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PreDestroy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +21,28 @@ import org.springframework.stereotype.Service;
 @Service
 @Scope("prototype")
 public class SavingBlockListener implements BlockListener {
+
+    private class BlockSaverThread extends Thread {
+        public boolean running = true;
+
+        public BlockSaverThread() {
+            setName("BlockSaver");
+        }
+
+        @Override
+        public void run() {
+            while (running) {
+                try {
+                    Block block = blockQueue.poll(500, TimeUnit.MILLISECONDS);
+                    if (block != null) {
+                        saveBlock(block);
+                    }
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+        }
+    }
 
     private static final Logger LOG = LoggerFactory.getLogger(SavingBlockListener.class);
 
@@ -28,8 +55,26 @@ public class SavingBlockListener implements BlockListener {
     @Autowired
     private TransactionService txService;
 
-    @Override
-    public void blockCreated(Block block) {
+    private ArrayBlockingQueue<Block> blockQueue;
+
+    private final BlockSaverThread blockSaver;
+
+    public SavingBlockListener() {
+        blockQueue = new ArrayBlockingQueue<>(1000);
+        blockSaver = new BlockSaverThread();
+        blockSaver.start();
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        blockSaver.running = false;
+    }
+
+    private void saveBlock(Block block) {
+        if (true) {
+            return;
+        }
+        LOG.debug("Persisting block #" + block.getNumber());
         blockDAO.save(block);
         if (!block.getTransactions().isEmpty()) {
             for (String txId : block.getTransactions()) {
@@ -40,6 +85,15 @@ public class SavingBlockListener implements BlockListener {
                     LOG.warn("Failed to load transaction details for tx " + txId, e);
                 }
             }
+        }
+    }
+
+    @Override
+    public void blockCreated(Block block) {
+        try {
+            blockQueue.put(block);
+        } catch (InterruptedException e) {
+            return;
         }
     }
 
