86d36edf2b4a62c1214794154457a2d86c9e8709
Chetan Sarva <chetan.sarva@jpmchase.com>
2016-05-25 17:31:06 -0400

    fix invalid gist loading

--- cakeshop-api/src/main/webapp/js/sandbox/gist.js
+++ cakeshop-api/src/main/webapp/js/sandbox/gist.js
@@ -16,38 +16,31 @@
         	return null;
         }
 
-        var loadingFromGist = false;
+    	var key = queryParams.gist;
+        if (!key) {
+            return false;
+        }
 
-        	var gistId;
-        	var key = queryParams.gist;
-        	if (key === '') {
-        		var str = prompt("Enter the URL or ID of the Gist you would like to load.");
-        		if (str !== '') {
-        			gistId = getGistId(str);
-        			loadingFromGist = !!gistId;
-        		}
-        	} else {
-        		gistId = getGistId(key);
-        		loadingFromGist = !!gistId;
-        	}
-        	$.ajax({
-        		url: 'https://api.github.com/gists/' + gistId,
-        		jsonp: 'callback',
-        		dataType: 'jsonp',
-                success: function(response) {
-                    if (response.data) {
-                        for (var key in response.data.files) {
-                            var content = response.data.files[key].content;
-                            if (Sandbox.Filer.get(key) === content) {
-                                Sandbox.activateTab(key);
-                                return;
-                            }
-                            var fname = Sandbox.Filer.getUniqueKey(key);
-                            Sandbox.addFileTab(fname, content, true);
+		var gistId = getGistId(key);
+		var loadingFromGist = !!gistId;
+    	$.ajax({
+    		url: 'https://api.github.com/gists/' + gistId,
+    		jsonp: 'callback',
+    		dataType: 'jsonp',
+            success: function(response) {
+                if (response.data) {
+                    for (var key in response.data.files) {
+                        var content = response.data.files[key].content;
+                        if (Sandbox.Filer.get(key) === content) {
+                            Sandbox.activateTab(key);
+                            return;
                         }
+                        var fname = Sandbox.Filer.getUniqueKey(key);
+                        Sandbox.addFileTab(fname, content, true);
                     }
                 }
-        	});
+            }
+    	});
 
         return loadingFromGist;
     };
