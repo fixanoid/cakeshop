928f00113e878d181b13262009efac918ae6aa9e
fixanoid <felix.shnir@jpmorgan.com>
2016-06-15 15:51:59 -0400

    Adding Oracle example

--- cakeshop-api/src/main/webapp/js/sandbox/lib.js
+++ cakeshop-api/src/main/webapp/js/sandbox/lib.js
@@ -5,33 +5,34 @@
 
 (function() {
 
-    var Sandbox = window.Sandbox = window.Sandbox || {};
+	var Sandbox = window.Sandbox = window.Sandbox || {};
 
-    Sandbox.loadContract = function(name) {
-        var url = "js/sandbox/contracts/" + name;
-        return new Promise(function(resolve, reject) {
-            $.get(url).done(function(data) {
-                resolve(data);
-            });
-        });
-    };
+	Sandbox.loadContract = function(name) {
+		var url = "js/sandbox/contracts/" + name;
+		return new Promise(function(resolve, reject) {
+			$.get(url).done(function(data) {
+				resolve(data);
+			});
+		});
+	};
 
-    $("#libView a.contract").click(function(e) {
-        var name = $(e.target).attr("data-name");
-        Sandbox.loadContract(name + ".txt").then(function(source) {
-            if (!source) {
-                console.log("no source?!");
-                return;
-            }
+	$("#libView a.contract").click(function(e) {
+		var name = $(e.target).data("name");
 
-            var fname = name;
-            var i = 0;
-            while (Sandbox.Filer.get(fname) !== undefined) {
-                i++;
-                fname = name + " " + i;
-            }
-            Sandbox.addFileTab(fname, source, true);
-        });
-    });
+		Sandbox.loadContract(name + ".txt").then(function(source) {
+			if (!source) {
+				console.log("no source?!");
+				return;
+			}
+
+			var fname = name;
+			var i = 0;
+			while (Sandbox.Filer.get(fname) !== undefined) {
+				i++;
+				fname = name + " " + i;
+			}
+			Sandbox.addFileTab(fname, source, true);
+		});
+	});
 
 })();
