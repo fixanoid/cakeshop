031ec1abba3781f593232ad680512854361301a7
Michael Kazansky <michael.kazansky@jpmchase.com>
2016-09-21 16:20:25 -0400

    removed not needed param in killProcessWin method

--- cakeshop-api/src/main/java/com/jpmorgan/ib/caonpd/cakeshop/util/ProcessUtils.java
+++ cakeshop-api/src/main/java/com/jpmorgan/ib/caonpd/cakeshop/util/ProcessUtils.java
@@ -114,7 +114,7 @@ public class ProcessUtils {
     }
 
     public static boolean killProcess(String pid, String exeName) throws InterruptedException, IOException {
-        boolean killed = SystemUtils.IS_OS_WINDOWS ? killProcessWin(pid, exeName) : killProcessNix(pid);
+        boolean killed = SystemUtils.IS_OS_WINDOWS ? killProcessWin(pid) : killProcessNix(pid);
         if (!killed) {
             return false;
         }
@@ -128,7 +128,7 @@ public class ProcessUtils {
         }
     }
 
-    public static boolean killProcessWin(String pid, String exeName) throws InterruptedException, IOException {
+    public static boolean killProcessWin(String pid) throws InterruptedException, IOException {
         Runtime.getRuntime().exec("taskkill /F /PID " + pid).waitFor();
         return true;
     }
