6caf02a434c3edb4a01a5498ffc78f22c533fd31
Chetan Sarva <chetan.sarva@jpmchase.com>
2016-10-21 11:56:22 -0400

    [cleanup] remove license header template

--- cakeshop-api/src/test/java/com/jpmorgan/ib/caonpd/cakeshop/test/config/TestCassandraConfig.java
+++ cakeshop-api/src/test/java/com/jpmorgan/ib/caonpd/cakeshop/test/config/TestCassandraConfig.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jpmorgan.ib.caonpd.cakeshop.test.config;
 
 import com.jpmorgan.ib.caonpd.cakeshop.cassandra.entity.Account;
