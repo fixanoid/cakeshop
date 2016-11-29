17114f66d0c6ee27ae989d087c7c6e864d69a0f2
Chetan Sarva <chetan@pixelcop.net>
2016-11-29 16:23:12 -0500

    [build] fixed several issues relating to clean build
    * added generated files to `mvn clean` targets
    * fixed gulp build order (async task issue)
    * fixed mvn frontend build ordering

--- cakeshop-client-js/src/main/javascript/gulpfile.js
+++ cakeshop-client-js/src/main/javascript/gulpfile.js
@@ -8,38 +8,42 @@ var watch       = require("gulp-watch");
 var batch       = require("gulp-batch");
 var plumber     = require("gulp-plumber");
 
+// write compiled dist files into `target/dist`
+var dest = "../../../target/dist/";
+var libs = "lib/**/*.js";
+
 gulp.task("default", ["compile"]);
 
-gulp.task("compile", function() {
-
-    var dest = "dist/";
-    var libs = "lib/**/*.js";
-
-    gulp.src(libs)
-        .pipe(concatenate("cakeshop.js"))
-        .pipe(gulp.dest(dest))
-        .pipe(concatenate("cakeshop-min.js"))
-        .pipe(minify())
-        .pipe(gulp.dest(dest));
-
-    gulp.src(bower().concat(libs))
-        .pipe(concatenate("cakeshop-combined.js"))
-        .pipe(gulp.dest(dest))
-        .pipe(concatenate("cakeshop-combined-min.js"))
-        .pipe(minify())
-        .pipe(gulp.dest(dest));
-
+gulp.task("compile-libs", function() {
+  return gulp.src(libs)
+    .pipe(concatenate("cakeshop.js"))
+    .pipe(gulp.dest(dest))
+    // .pipe(gulp.dest("../../../../cakeshop-api/src/main/webapp/js/vendor/"))
+    .pipe(concatenate("cakeshop-min.js"))
+    .pipe(minify())
+    .pipe(gulp.dest(dest));
 });
 
+gulp.task("compile-combined", function() {
+  return gulp.src(bower().concat(libs))
+    .pipe(concatenate("cakeshop-combined.js"))
+    .pipe(gulp.dest(dest))
+    .pipe(concatenate("cakeshop-combined-min.js"))
+    .pipe(minify())
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task("compile", ["compile-libs", "compile-combined"]);
+
 gulp.task("install", ["compile"], function() {
-    gulp.src("dist/cakeshop.js")
-        .pipe(concatenate("cakeshop.js"))
-        .pipe(gulp.dest("../../../../cakeshop-api/src/main/webapp/js/vendor/"));
+  return gulp.src(dest+"cakeshop.js")
+    // .pipe(concatenate("cakeshop.js"))
+    .pipe(gulp.dest("../../../../cakeshop-api/src/main/webapp/js/vendor/"));
 });
 
 gulp.task("watch", ["install"], function() {
-    watch("lib/**/*.js", batch(function (events, done) {
-        gutil.log("-- recompiling on file change --");
-        gulp.start("install", done);
-    }));
+  watch("lib/**/*.js", batch(function (events, done) {
+    gutil.log("-- recompiling on file change --");
+    gulp.start("install", done);
+  }));
 });
