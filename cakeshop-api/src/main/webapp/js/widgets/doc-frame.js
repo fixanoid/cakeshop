32c3f560c3d671bab8ef86d9e47c1cfee8c95e5b
fixanoid <felix.shnir@gmail.com>
2016-11-30 19:37:59 -0500

    #20 Prefer larger original size for the api-doc

--- cakeshop-api/src/main/webapp/js/widgets/doc-frame.js
+++ cakeshop-api/src/main/webapp/js/widgets/doc-frame.js
@@ -12,12 +12,16 @@ module.exports = function() {
 			'	<iframe src="api.html" style="width: 100%; height: 550px;" scrolling="yes" marginwidth="0" marginheight="0" frameborder="0" vspace="0" hspace="0"></iframe>' +
 			'</div>'),
 
+		getVisible: function() {
+			return $(window).height() - 350;
+		},
+
 		render: function() {
 			$('#grounds').append(this.shell.tpl);
 
 			$('#widget-' + this.shell.id)
 				.css({
-					'height': '600px',
+					'height': (this.getVisible() > 600 ? this.getVisible() : '600') + 'px',
 					'margin-bottom': '10px',
 					'overflow': 'auto'
 				})
