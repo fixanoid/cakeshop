6a3f092a00324e2261a94ee745f10a2efde243a5
Chetan Sarva <chetan@pixelcop.net>
2016-12-06 22:54:19 -0500

    fix

--- cakeshop-api/src/main/webapp/js/sandbox/gist.js
+++ cakeshop-api/src/main/webapp/js/sandbox/gist.js
@@ -8,25 +8,25 @@
     Sandbox.loadFromGist = function(queryParams) {
 
         function getGistId(str) {
-        	var idr = /[0-9A-Fa-f]{8,}/;
-        	var match = idr.exec(str);
+          var idr = /[0-9A-Fa-f]{8,}/;
+          var match = idr.exec(str);
             if (match) {
                 return match[0];
             }
-        	return null;
+          return null;
         }
 
-    	var key = queryParams.gist;
+      var key = queryParams.gist;
         if (!key) {
             return false;
         }
 
-		var gistId = getGistId(key);
-		var loadingFromGist = !!gistId;
-    	$.ajax({
-    		url: 'https://api.github.com/gists/' + gistId,
-    		jsonp: 'callback',
-    		dataType: 'jsonp',
+    var gistId = getGistId(key);
+    var loadingFromGist = !!gistId;
+      $.ajax({
+        url: 'https://api.github.com/gists/' + gistId,
+        jsonp: 'callback',
+        dataType: 'jsonp',
             success: function(response) {
                 if (response.data) {
                     for (var key in response.data.files) {
@@ -40,7 +40,7 @@
                     }
                 }
             }
-    	});
+      });
 
         return loadingFromGist;
     };
